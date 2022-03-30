8d5a89ff8a986bdd09b70cdfab45aaeda752ddbc
Hau Nguyen <nch180297@gmail.com>
2022-03-30 09:33:33 +0700

    update meta

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -36,7 +36,7 @@ function AppRoot({ Component, emotionCache = clientSideEmotionCache, pageProps, 
       <Head>
         <meta
           name="viewport"
-          content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
+          content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover"
         />
       </Head>
       {/* #TODO: Change cluster to "production" when we have a prod env */}
